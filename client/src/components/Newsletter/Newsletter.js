cccee69266d33d5817177cb00ed1fcc533af237e
Luke Graham <luke.evan.graham@gmail.com>
2021-11-29 17:40:42 -0800

    more styling of newsletter

--- client/src/components/Newsletter/Newsletter.js
+++ client/src/components/Newsletter/Newsletter.js
@@ -30,17 +30,61 @@ Font.register({
   ],
 });
 
+
 const styles = StyleSheet.create({
   page: {
+    padding: "20 20 60 20"
+  },
+  title: {
+    fontFamily: "Assistant",
+    fontSize: 20,
+    // marginBottom: 7
+  },
+  text: {
+    fontSize: 12,
+    fontFamily: "Cardo",
+    margin: "0 0 12 0",
+    lineHeight: "1.5"
+  },
+  image: {
+    maxWidth: "50%",
+    marginBottom: 7
+  },
+  pageNumber: {
+    position: "absolute",
+    fontSize: 12,
+    bottom: 30,
+    left: 0,
+    right: 0,
+    textAlign: 'center',
+    color: 'grey',
+    fontFamily: "Assistant"
+  },
+  article: {
+    borderBottom: "1 solid grey",
+    marginBottom: 10
+    // backgroundColor: "red"
+  },
+  shortArticle: {
     flexDirection: "row",
-    backgroundColor: "#fff",
-    flexWrap: "wrap",
+    borderBottom: "1 solid grey",
+    marginBottom: 10,
+    paddingBottom: "12px"
   },
-  section: {
-    margin: 10,
-    padding: 10,
-    flexGrow: 1,
+  shortArticleText: {
+    marginRight: 185,
+    marginLeft: 10,
+    fontSize: 12,
+    // flexGrow: "0",
+    fontFamily: "Cardo",
+    lineHeight: "1.5"
   },
+  shortArticleImage: {
+    width: "33%",
+    height: "auto",
+    // flexGrow: "0",
+    // flexShrink : "0"
+  }
 });
 
 const Newsletter = ({ orgName, articles }) => {
@@ -48,6 +92,11 @@ const Newsletter = ({ orgName, articles }) => {
     family: "Roboto",
     src: "https://fonts.googleapis.com/css2?family=Assistant&display=swap",
   });
+
+  const longArticles = articles[0].filter(article => article.text.length >= 400)
+  const shortArticles = articles[0].filter(article => article.text.length > 400)
+
+
   return (
     <Document>
       <Page size="LETTER" style={styles.page}>
@@ -59,8 +108,8 @@ const Newsletter = ({ orgName, articles }) => {
             flexDirection: "row",
             justifyContent: "space-between",
             flexBasis: "97%",
-            margin: 10,
-            padding: 10,
+            margin: "0 10",
+            padding: "10",
             fontSize: 11,
           }}
         >
@@ -75,10 +124,9 @@ const Newsletter = ({ orgName, articles }) => {
         </View>
         <View
           style={{
-            width: "97%",
-            margin: 10,
+            // width: "100%",
+            margin: "10 0",
             padding: 10,
-            borderBottom: "1 solid grey",
           }}
         >
           <Text
@@ -90,43 +138,64 @@ const Newsletter = ({ orgName, articles }) => {
               letterSpacing: 3,
               fontFamily: "Assistant",
               fontWeight: 200,
+              borderBottom: "1 solid grey",
+              marginBottom: 10
             }}
           >
             Newsletter
           </Text>
-        </View>
-        <View style={styles.section}>
-          {articles[0].map((article) => (
-            <>
-              {console.log("ART: ", article)}
-              <Image
-                style={{ width: "100mm", marginBottom: "3mm" }}
-                src={`https://cors.bridged.cc/${article.image.src}`}
-              />
-              <Text
-                style={{
-                  fontWeight: "200",
-                  fontSize: "6mm",
-                  fontFamily: "Assistant",
-                }}
-              >
-                {article.headline}
-              </Text>
-              {article.text.split("\n").map((text, index) =>
-                index > 0 ? (
-                  <Text
-                    style={{ fontFamily: "Cardo", fontSize: "4.5mm", marginBottom: "3mm" }}
-                    key={index}
-                  >
-                    {text}
+
+          {articles[0].map((article) => {
+            console.log("article", article)
+            if (article.image.src) {
+              if (article.text.length <= 400) {
+                return (
+                  <View style={styles.shortArticle}>
+                    <Image style={styles.shortArticleImage} src={`https://cors-anywhere.herokuapp.com/${article.image.src}`} />
+                    <View style={styles.shortArticleText}>
+                      <Text style={styles.title}>{article.headline[0].length >= 100 ? null : (article.headline)}</Text>
+                      <Text>{article.headline[0].length >= 100 ? article.text.split("\n") : article.text.split("\n").slice(1)}</Text>
+                    </View>
+                  </View>
+                )
+              } else {
+                return (
+                  <View style={styles.article}>
+                    <Image style={styles.image} src={`https://cors-anywhere.herokuapp.com/${article.image.src}`} />
+                    {/* If headline is less than 100 words show it with heading */}
+                    <Text style={styles.title}>{article.headline[0].length >= 100 ? null : (article.headline)}</Text>
+                    {article.text.split("\n").map((text, index) =>
+                      index > 0 && text.length > 1 ? (
+                        <Text style={styles.text}>{text}</Text>
+                      ) :
+                        text.length >= 100 ? (
+                          <Text style={styles.text}>{text}</Text>
+                        ) : null
+                    )}
+                  </View>
+                )
+              }
+            } else {
+              console.log("NO IMAGE!")
+              return (
+
+                <View style={styles.article}>
+                  <Text style={styles.text}>
+                    {article.text.split("\n")}
                   </Text>
-                ) : null
-              )}
-            </>
-          ))}
+                </View>
+              )
+
+
+
+            }
+          })}
         </View>
+        <Text style={styles.pageNumber} render={({ pageNumber, totalPages }) => (
+          `${pageNumber} / ${totalPages}`
+        )} fixed></Text>
       </Page>
-    </Document>
+    </Document >
   );
 };
 
